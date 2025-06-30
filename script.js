5f97ff8c0e9eb75a610b86b4ea5e51d15b4a8690
vinay1723 <144577819+vinay1723@users.noreply.github.com>
2025-06-30 15:53:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
 //your JS code here. If required.
+let form = document.querySelector('form');
+let name = document.getElementById('name');
+let age = document.getElementById('age');
+
+form.addEventListener('submit',(e) => {
+	e.preventDefault();
+	if (!name.value || !age.value) {
+		alert('Please enter valid details.')
+		return;
+	}
+     let ageNum = Number(age.value);
+	let promise = new Promise((resolve,reject) => {
+
+		setTimeout(() => {
+		if (ageNum >= 18) {
+				resolve("Welcome, . You can vote.")
+		}else {
+			resolve("Oh sorry . You aren't old enough.")
+		}
+		},4000)
+	});
+
+	promise.then((data) => {
+		alert(data);
+	}).catch((err) => {
+		alert(err);
+	})
+})
